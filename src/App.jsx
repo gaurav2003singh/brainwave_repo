5c9aeb9ee0cd41a20ec83a2db4c242293798f15e
Gaurav Singh <www.gschauhan2003@gmail.com>
2024-12-02 17:52:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl">hello gaurav</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <button className="mt-10" href="#login">
+           
+        </button>
+      </div>
+      <ButtonGradient />
+
+
+
+    </>
+  );
+};
+
+export default App;
